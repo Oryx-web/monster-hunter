bf5ddbae7360e968daceedeb90d4a07291060c76
Holman Nieto <holmannieto33@gmail.com>
2025-03-17 21:24:19 -0500

    images

--- src/pages/Weapons.jsx
+++ src/pages/Weapons.jsx
@@ -28,7 +28,6 @@ export default function Weapons() {
     useEffect(() => {
         if (selectedWeapon)
             setSelectedWeapon(selectedWeapon);
-            console.log(selectedWeapon);
       }, [selectedWeapon]);
 
     useEffect(() => {
