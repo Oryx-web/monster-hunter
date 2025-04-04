5fe1d5d7921b6c2e338b175aa63d0ddde83fc78e
Holman Nieto <holmannieto33@gmail.com>
2025-04-04 00:28:02 -0500

    Weapon tree branches added

--- src/components/Weapons/WeaponList.jsx
+++ src/components/Weapons/WeaponList.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 
 const WeaponList = ({ weapons, handleClickWeapon, selectedWeapon }) => {
     const baseWeapons = weapons.filter(weapon => !weapon.crafting?.previous);
@@ -23,16 +23,16 @@ const WeaponList = ({ weapons, handleClickWeapon, selectedWeapon }) => {
 
 const WeaponNode = ({ weapon, weapons, handleClickWeapon, selectedWeapon}) => {
     const nodeRef = useRef(null);
+    const childRefs = useRef([]);
+
     const childWeapons = weapon.crafting?.branches
         ? weapons.filter(w => weapon.crafting.branches.includes(w.id))
         : [];
 
     return (
-        <div
-            className="flex items-center justify-center relative weapon-node"
-            ref={nodeRef}
-        >
-            {/* Nodo principal */}
+        <div className="flex items-center justify-center relative weapon-node" ref={nodeRef}>
+            
+            {/* Weapon Icon */}
             <div
                 className={`weapon-card flex flex-col items-center cursor-pointer hover:scale-105 ${selectedWeapon?.id === weapon.id ? "glow" : ""}`}
                 onClick={() => handleClickWeapon(weapon)}
@@ -46,24 +46,54 @@ const WeaponNode = ({ weapon, weapons, handleClickWeapon, selectedWeapon}) => {
                 <p className="hidden sm:block sm:text-xs">{weapon.name}</p>
             </div>
 
-            {/* Contenedor de hijos */}
+            {/* Vertical Line to Parent Weapon */}
             {childWeapons.length > 0 && (
-                <div className={`grid grid-rows-${childWeapons.length} gap-8`}>
-                    {childWeapons.map((childWeapon, index) => (
-                        <div key={childWeapon.id} className="relative flex items-center">
-                            {/* Línea horizontal a los hijos, pero no al último */}
-                            {index < childWeapons.length && (
-                                <div className="relative w-4 h-[2px] bg-white child-node"></div>
-                            )}
+                <div className="relative w-3 h-[2px] bg-white"></div>
+            )}
 
-                            <WeaponNode
-                                weapon={childWeapon}
-                                weapons={weapons}
-                                handleClickWeapon={handleClickWeapon}
-                                selectedWeapon={selectedWeapon}
-                            />
-                        </div>
-                    ))}
+            {/* Connector Lines */}
+            {childWeapons.length > 0 && (
+                <div className={`relative w-full flex h-full`}>
+                    {/* Child Weapon Nodes */}
+                    <div
+                        className={`grid grid-rows-${childWeapons.length} gap-4 relative grid-equal-rows ${childWeapons.length > 1 && "relative"}`}
+                    >
+                        { childWeapons.length === 2 && (
+                            
+                            <div
+                                className="absolute top-0 h-[53.5%] w-[3px] translate-y-[43.5%] pointer-events-none"
+                            >
+                                <div className="h-full bg-white w-full"></div>
+                            </div>
+                        )}
+
+                        {childWeapons.length === 3 && (
+                            <div
+                                className="absolute top-0 h-[67.9%] w-[3px] translate-y-[23.7%] pointer-events-none"
+                            >
+                                <div className="h-full bg-white w-full"></div>
+                            </div>
+                        )}
+
+                        {childWeapons.map((childWeapon, index) => (
+                            <div
+                                key={childWeapon.id}
+                                className="relative flex items-center"
+                                ref={(el) => (childRefs.current[index] = el)}
+                            >
+                                {/* Horizontal Line Connecting to Child */}
+                                <div className="relative w-3 h-[2px] bg-white"></div>
+
+                                <WeaponNode
+                                    weapon={childWeapon}
+                                    weapons={weapons}
+                                    handleClickWeapon={handleClickWeapon}
+                                    selectedWeapon={selectedWeapon}
+                                />
+                                
+                            </div>
+                        ))}
+                    </div>
                 </div>
             )}
         </div>
