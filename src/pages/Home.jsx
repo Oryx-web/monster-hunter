089b2a2d819671bb68d66032618fee4fb01b5d0b
Holman Nieto <holmannieto33@gmail.com>
2025-04-08 04:34:43 -0500

    home redesign in progress!

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,11 +18,45 @@ export default function Home() {
       <div className="sticky top-0 pb-[80%] bg-cover bg-no-repeat sm:pb-[55%]" style={{ backgroundImage: `url('${import.meta.env.BASE_URL}banner_home.webp')`}}>
       </div>
 
-      <div className="flex flex-col items-center justify-center mt-[-12%] sm:mt-[-10%]">
+      <div className="flex flex-col items-center justify-center pb-[5%] mt-[-12%] sm:mt-[-10%]">
         <img className="sticky w-[25%] z-[30] sm:w-[15%]" src={`${import.meta.env.BASE_URL}/middle_logo.png`} alt="Weapons_image" loading="lazy" />
 
         <div className="mt-[-15%] flex flex-col z-10 sm:mt-[-8%]">
           <div className="ripped pt-[15%] w-full flex flex-col items-center justify-center justify-items-center bg-cover bg-no-repeat z-[20]" style={{ backgroundImage: `url('${import.meta.env.BASE_URL}home_monsters.webp')`}}>
+            <div className="w-full pb-[5%] flex items-center justify-center justify-items-center bg-cover bg-no-repeat">
+              <img className="w-[20%] pb-[8%] pt-[2%] sm:p-0 z-10" src={`${import.meta.env.BASE_URL}/logo.png`} alt="X" />
+              <div>
+                <h1 className="title text-center font-semibold [text-shadow:_0px_8px_5px_rgb(0_0_0_/_100%)]">INTRODUCTION</h1>
+                <div className="w-[100%] p-[10%] rounded-lg flex flex-col items-center justify-center">
+                  <p className="intro text-white text-lg font-medium tracking-wide">Welcome, Hunter! Access detailed information about large and small monsters, weapons and armors.</p>
+                </div>
+              </div>
+              <img className="w-[20%] pb-[8%] pt-[2%] sm:p-0 z-10" src={`${import.meta.env.BASE_URL}/logo.png`} alt="X" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
+        <div className="mt-[-15%] flex flex-col z-10 sm:mt-[-10%]">
+          <div className="ripped pt-[15%] w-full flex flex-col items-center justify-center justify-items-center bg-cover bg-no-repeat z-[20]" style={{ backgroundImage: `url('${import.meta.env.BASE_URL}home_monsters.webp')`}}>
+            <div className="w-full flex items-center justify-center justify-items-center bg-cover bg-no-repeat">
+              <img className="w-[20%] pb-[8%] pt-[2%] sm:p-0 z-10" src={`${import.meta.env.BASE_URL}/logo.png`} alt="X" />
+              <div>
+                <h1 className="title text-center font-semibold [text-shadow:_0px_8px_5px_rgb(0_0_0_/_100%)]">INTRODUCTION</h1>
+                <div className="w-[100%] p-[10%] rounded-lg flex flex-col items-center justify-center">
+                  <p className="intro text-white text-lg font-medium tracking-wide">Welcome, Hunter! Access detailed information about large and small monsters, weapons and armors.</p>
+                </div>
+              </div>
+              <img className="w-[20%] pb-[8%] pt-[2%] sm:p-0 z-10" src={`${import.meta.env.BASE_URL}/logo.png`} alt="X" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
+        <div className="mt-[-15%] flex flex-col z-10 sm:mt-[-10%]">
+          <div className="ripped mt-[5%] pt-[15%] w-full flex flex-col items-center justify-center justify-items-center bg-cover bg-no-repeat z-[20]" style={{ backgroundImage: `url('${import.meta.env.BASE_URL}home_monsters.webp')`}}>
             <div className="w-full flex items-center justify-center justify-items-center bg-cover bg-no-repeat">
               <img className="w-[20%] pb-[8%] pt-[2%] sm:p-0 z-10" src={`${import.meta.env.BASE_URL}/logo.png`} alt="X" />
               <div>
