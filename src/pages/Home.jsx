9a42be3bfdc88063eb78234ef652b5a2a042172c
Holman Nieto <holmannieto33@gmail.com>
2025-04-08 04:17:47 -0500

    home redesign in progress!

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,39 @@
 export default function Home() {
   return(
-    <div className="min-h-screen min-w-screen py-12 px-5 text-[#54361E] bg-cover bg-no-repeat flex flex-col justify-center items-center text-xs md:text-base md:pt-0" style={{ backgroundImage: `url('${import.meta.env.BASE_URL}background.webp')` }}>
-      <h1 className="text-5xl font-serif mb-6 border-b-4 border-[#7B4F2D] pb-2">Hunter's Notes</h1>
-      <p className="text-lg text-center max-w-xl mb-8">
-        Welcome, Hunter! Access detailed information about large and small monsters, weapons and armors.
-      </p>
-      <div className="flex flex-col md:flex-row items-center justify-items-center">
-        <div className="items-center justify-items-center mb-8 md:m-5">
-          <div className="grid grid-cols-2 m-5">
-            <img className="w-32" src={`${import.meta.env.BASE_URL}/icons/MHW-Rathalos_Icon.svg`} alt="Monsters_image" loading="lazy" />
-            <img className="w-32" src={`${import.meta.env.BASE_URL}/icons/MHW-Rathian_Icon.svg`} alt="Monsters_image" loading="lazy" />
-          </div>
-          <a href="/monster-hunter/#/monsters" className="px-6 py-3 bg-[#7B4F2D] !text-white text-xl rounded-lg shadow-md hover:bg-[#54361E] transition-transform hover:scale-105">
-            View Monsters
-          </a>
-        </div>
+    <div className="min-h-screen min-w-screen text-xs"> 
+      <div className="h-fit fixed top-0 pt-[8%] w-full flex items-center justify-evenly z-[50] backdrop-blur-[3px] mt-[5%] sm:m-0 sm:p-0">
+        <img
+          className="hidden w-[25%] drop-shadow-[0_8px_5px_rgba(0,0,0,1)] sm:p-0 sm:block"
+          src={`${import.meta.env.BASE_URL}/logo.png`}
+          alt="X"
+        />
+        <nav className="flex flex-col items-center gap-2 sm:flex-row md:gap-7">
+          <a className="text-white text-lg font-medium tracking-wide drop-shadow-[0_6px_3px_rgba(0,0,0,1)]" href="1">Home</a>
+          <a className="text-white text-lg font-medium tracking-wide drop-shadow-[0_6px_3px_rgba(0,0,0,1)]" href="2">Monsters</a>
+          <a className="text-white text-lg font-medium tracking-wide drop-shadow-[0_6px_3px_rgba(0,0,0,1)]" href="3">Weapons</a>
+          <a className="text-white text-lg font-medium tracking-wide drop-shadow-[0_6px_3px_rgba(0,0,0,1)]" href="4">Armors</a>
+        </nav>
+      </div>
+      
+      <div className="sticky top-0 pb-[80%] bg-cover bg-no-repeat sm:pb-[55%]" style={{ backgroundImage: `url('${import.meta.env.BASE_URL}banner_home.webp')`}}>
+      </div>
 
-        <div className="items-center justify-items-center my-8 md:border-r-2 md:border-l-2">
-          <div className="grid grid-cols-4 m-5">
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Bow_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Charge_Blade_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Dual_Blades_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Great_Sword_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Gunlance_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Hammer_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Heavy_Bowgun_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Hunting_Horn_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Insect_Glaive_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Long_Sword_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Switch_Axe_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Sword_and_Shield_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-          </div>
-          <a href="/monster-hunter/#/weapons" className="px-6 py-3 bg-[#7B4F2D] !text-white text-xl rounded-lg shadow-md hover:bg-[#54361E] transition-transform hover:scale-105">
-            View Weapons
-          </a>
-        </div>
+      <div className="flex flex-col items-center justify-center mt-[-12%] sm:mt-[-10%]">
+        <img className="sticky w-[25%] z-[30] sm:w-[15%]" src={`${import.meta.env.BASE_URL}/middle_logo.png`} alt="Weapons_image" loading="lazy" />
 
-        <div className="items-center justify-items-center my-8 md:m-5">
-          <div className="grid grid-cols-3 m-5">
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Helmet_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1 translate-y-8" src={`${import.meta.env.BASE_URL}/home/Chest_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Arm_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1" src={`${import.meta.env.BASE_URL}/home/Waist_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
-            <img className="w-20 m-1 col-start-3" src={`${import.meta.env.BASE_URL}/home/Leg_Icon_White.svg`} alt="Weapons_image" loading="lazy" />
+        <div className="mt-[-15%] flex flex-col z-10 sm:mt-[-8%]">
+          <div className="ripped pt-[15%] w-full flex flex-col items-center justify-center justify-items-center bg-cover bg-no-repeat z-[20]" style={{ backgroundImage: `url('${import.meta.env.BASE_URL}home_monsters.webp')`}}>
+            <div className="w-full flex items-center justify-center justify-items-center bg-cover bg-no-repeat">
+              <img className="w-[20%] pb-[8%] pt-[2%] sm:p-0 z-10" src={`${import.meta.env.BASE_URL}/logo.png`} alt="X" />
+              <div>
+                <h1 className="title text-center font-semibold [text-shadow:_0px_8px_5px_rgb(0_0_0_/_100%)]">INTRODUCTION</h1>
+                <div className="w-[100%] p-[10%] rounded-lg flex flex-col items-center justify-center">
+                  <p className="intro text-white text-lg font-medium tracking-wide">Welcome, Hunter! Access detailed information about large and small monsters, weapons and armors.</p>
+                </div>
+              </div>
+              <img className="w-[20%] pb-[8%] pt-[2%] sm:p-0 z-10" src={`${import.meta.env.BASE_URL}/logo.png`} alt="X" />
+            </div>
           </div>
-          <a href="/monster-hunter/#/armors" className="px-6 py-3 bg-[#7B4F2D] !text-white text-xl rounded-lg shadow-md hover:bg-[#54361E] transition-transform hover:scale-105">
-            View Armors
-          </a>
         </div>
       </div>
     </div>
